ab498b5603747f04412bb26d7b1cc6bd56c3d21d
antonio <antonio@pykiss.com>
2020-09-01 23:16:20 +0200

    docs

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b6598db9ff8d439cfc72";
+/******/ 	var hotCurrentHash = "3d912053b1f9a9dd3391";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- docs/_next/static/jmwfoNoC6a6wQgvX9PI6a/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Frequired","\u002Fbasic","\u002Fmutation"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
